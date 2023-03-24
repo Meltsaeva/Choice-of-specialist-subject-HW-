f9ba8ba7e68febbe87ab1e7d3991f679ca47478a
Anastasiia <Batrakovaad@yndex.ru>
2023-03-24 14:33:34 +0700

    finitask

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input array length");
+int array_length = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[array_length];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Input array");
+    array[i] = Console.ReadLine();
+}
+
+
+string[] Array_clone = new string[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+    Array.Resize(ref Array_clone, Array_clone.Length + 1);
+    Array_clone[Array_clone.Length - 1] = array[i];
+    }
+}
+var str = string.Join(" ", Array_clone);
+Console.WriteLine(str);
